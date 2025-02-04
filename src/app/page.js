78dd37b3c1f9e0d89ae6fcf86069ebbd0fff9859
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2025-02-03 22:01:50 -0400

    atualizacao

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import styles from "../style/page.module.css";
-
+import Image from "next/image";
 export default function Home() {
 	return (
-		<div>
-			<p className={styles.p}>Início</p>
+		<div className={styles.container}>
+			{/* <Image>
+				
+			</Image> */}
+			<h1 className={styles.h1}>Bem-Vindo à VivaMed!</h1>
+			<p className={styles.p}>Cuidar da sua saúde é a nossa prioridade. <br/>Aqui, você encontra um ambiente acolhedor, profissionais dedicados e um atendimento personalizado para o seu bem-estar.<br/> Estamos prontos para oferecer o melhor em cuidado e atenção, porque a sua saúde merece excelência. <br/>Estamos felizes em ter você conosco! </p>
 		</div>
 	);
 }
--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -1,28 +1,33 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./header.module.css";
+import styles from "../../style/header.module.css"
 import React, { useState } from "react";
+import '../../style/reset.css';
 export default function Header() {
-	<header className="header">
-		<nav className={styles.navbar}>
-			<div>
-				<Image
-					src="/assets/logo.png"
-					width="30"
-					height="30"
-					alt="Logo Clínica"
-				></Image>
-				<h1 className="titulo">Clínica Médica</h1>
-				<ul>
-					<li>
-						<Link href="/">HOME</Link>
-						<Link href="/">MÉDICO</Link>
-						<Link href="/">PACIENTE</Link>
-						<Link href="/">CONSULTA</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	</header>;
+	return(
+		<header className="header">
+			<nav className={styles.navbar}>
+				<div className={styles.logo}>
+					<Image
+						src="/assets/edificio-hospitalar.png"
+						width="80"
+						height="80"
+						alt="Logo Clínica"
+					></Image>
+					<h1 className={styles.titulo}>Clínica Médica</h1>
+				</div>
+
+					<ul className={styles.link_items}>
+						<li>
+							<Link href="/">HOME</Link>
+							<Link href="/">MÉDICO</Link>
+							<Link href="/">PACIENTE</Link>
+							<Link href="/">CONSULTA</Link>
+						</li>
+					</ul>
+			</nav>
+	</header>
+	)
+	
 }
