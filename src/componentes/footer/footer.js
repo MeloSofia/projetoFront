2bd8112c6926e27c8b7009a219aff3e00da08836
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2025-02-05 21:11:23 -0400

    update

--- src/componentes/footer/footer.js
+++ src/componentes/footer/footer.js
@@ -1,8 +1,28 @@
 import styles from "../../style/footer.module.css"
+import Image from "next/image";
 export default function Footer(){
   return(
     <div className={styles.footer}>
-      <h1>Legenda</h1>
+      <div className={styles.legenda}>
+        <h1>VivaMed</h1>
+        <p className={styles.tituloFooter}>©2025 - VivaMed.</p>
+      </div>
+      <div className={styles.imageFooter}>
+        <Image
+        src="/assets/Doctors-bro.svg"
+        width="100"
+        height="100"
+        alt="Logo Footer">
+        </Image>
+      </div>
+      <div>
+        <Image 
+        src="/assets/Doctors-bro.svg"
+        width="100"
+        height="100"
+        alt="Logo Footer">
+        </Image>
+      </div>
     </div>
   )
 }
